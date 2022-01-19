48160b6b857a9f4bbf798c4d869a82d4869f436d
swapnilmahendru <swapnil.mahendru@geminisolutions.in>
2022-01-19 12:42:21 +0530

    inital commits

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  newName: String;
+  newEmail: String;
+  newPass: String;
 
   constructor() {}
-
+  sendData(a, b, c) {
+    localStorage.setItem('name', a);
+    localStorage.setItem('email', b);
+    localStorage.setItem('password', c);
+  }
+  getData() {
+    this.newName = localStorage.getItem('name');
+    this.newEmail = localStorage.getItem('email');
+    this.newPass = localStorage.getItem('password');
+  }
 }
